15263a56ab2a39879c603d1872aeb681cf4f30b9
sweetSupremum <siroezhka29@yandex.ru>
2024-06-15 02:12:57 +0400

    заебало то как все это если честно))

--- src/TimeGraph.jsx
+++ src/TimeGraph.jsx
@@ -4,16 +4,13 @@ const TimeGraph = function (props) {
 
     return (
         <>
-            <div className={"container pt-5"}>
-                <Graph
-                    imageName={props.imageName} ordinates={props.ordinates}
-                    abscissa={props.abscissa}
-                    name={props.name}
-                    abscissaName="t"
-                    countGraphs={props.countGraphs}
-                />
-            </div>
-
+            <Graph
+                imageName={props.imageName} ordinates={props.ordinates}
+                abscissa={props.abscissa}
+                name={props.name}
+                abscissaName="t"
+                countGraphs={props.countGraphs}
+            />
         </>
     )
 }
